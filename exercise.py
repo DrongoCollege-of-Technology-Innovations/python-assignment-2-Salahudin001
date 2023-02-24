ec230adcb60c90792d135d3e5cc815ce41c7e845
salahudin001 <salahudinadan2069@gmail.com>
2023-02-24 17:56:30 +0300

    reverse

--- exercise.py
+++ exercise.py
@@ -2,9 +2,20 @@
 
 def reverse():
     num = 123456789
+    reverse_n = 0
     reverse_number = 0
     # add your code here
-
+    
+    while num > 0:
+       n = num % 10
+    #    print(n)
+       reverse_number = (reverse_number * 10) +n
+    #    print(reverse_number)
+       num = num // 10
+    #    print(num)
+    #    print(reverse_number)
+    
+    print(reverse_number)
 
     return reverse_number
 
